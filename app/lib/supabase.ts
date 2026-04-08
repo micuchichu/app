4ff9087403bf1e3ac2d1b2ce0b69a057f037ac35
Iustin <iustin.icleanu@gmail.com>
2026-04-08 18:41:37 +0300

    6

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://rgbiqrhlafggkfbzedwi.supabase.co';
-const supabaseAnonKey = process.env.supabaseAnonKey!;
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
